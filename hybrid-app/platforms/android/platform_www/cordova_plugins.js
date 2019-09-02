4c89c36d23cbfc549cc10906607eb492bd457307
unknown <miroslav.nedelchev@dentacoin.com>
2019-09-02 15:17:12 +0300

    push file transfer cordova plugin

--- hybrid-app/platforms/android/platform_www/cordova_plugins.js
+++ hybrid-app/platforms/android/platform_www/cordova_plugins.js
@@ -221,6 +221,22 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "FilePicker"
       ]
+    },
+    {
+      "id": "cordova-plugin-file-transfer.FileTransferError",
+      "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
+      "pluginId": "cordova-plugin-file-transfer",
+      "clobbers": [
+        "window.FileTransferError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file-transfer.FileTransfer",
+      "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
+      "pluginId": "cordova-plugin-file-transfer",
+      "clobbers": [
+        "window.FileTransfer"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -230,6 +246,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-filechooser": "1.2.0",
     "phonegap-plugin-barcodescanner": "8.1.0",
     "cordova-plugin-network-information": "2.0.2",
-    "cordova-plugin-filepicker": "1.1.6"
+    "cordova-plugin-filepicker": "1.1.6",
+    "cordova-plugin-file-transfer": "1.7.1"
   };
 });
