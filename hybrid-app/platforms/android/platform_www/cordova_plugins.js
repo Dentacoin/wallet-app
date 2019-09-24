fbfb06c40a978ebda57a81da80bc663dd8323f0c
unknown <miroslav.nedelchev@dentacoin.com>
2019-09-24 14:54:46 +0300

    fix ios design bugs

--- hybrid-app/platforms/android/platform_www/cordova_plugins.js
+++ hybrid-app/platforms/android/platform_www/cordova_plugins.js
@@ -259,6 +259,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.StatusBar"
       ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -272,6 +280,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-file-transfer": "1.7.1",
     "es6-promise-plugin": "4.2.2",
     "cordova-plugin-x-socialsharing": "5.4.7",
-    "cordova-plugin-statusbar": "2.4.3"
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-splashscreen": "5.0.3"
   };
 });
