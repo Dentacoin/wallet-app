77935675cdfe8c151068086cb586ec96fb840279
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-18 07:33:06 +0200

    fix social metadata

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -13,11 +13,11 @@ import {Router} from '@angular/router';
 export class HomepageComponent implements AfterViewInit {
     constructor(private meta: Meta, private titleService: Title) {
         this.titleService.setTitle('Dentacoin Wallet App: Buy, Store & Manage Your DCN Tokens');
-        this.meta.updateTag({name: 'description', content: 'Dentacoin Wallet allows users to easily and securely store, send, receive DCN tokens, as well as to buy DCN with credit card and other cryptocurrencies.'});
-        this.meta.updateTag({name: 'keywords', content: 'buy dentacoin, store dentacoin, dentacoin wallet, pay with dentacoin'});
-        this.meta.updateTag({name: 'og:title', content: 'Dentacoin Wallet App: Buy, Store & Manage Your DCN Tokens'});
-        this.meta.updateTag({name: 'og:description', content: 'Dentacoin Wallet allows users to easily and securely store, send, receive DCN tokens, as well as to buy DCN with credit card and other cryptocurrencies.'});
-        this.meta.updateTag({name: 'og:image', content: 'https://wallet.dentacoin.com/assets/images/dentacoin-wallet-dapp.png'});
+        this.meta.updateTag({type: 'description', content: 'Dentacoin Wallet allows users to easily and securely store, send, receive DCN tokens, as well as to buy DCN with credit card and other cryptocurrencies.'});
+        this.meta.updateTag({type: 'keywords', content: 'buy dentacoin, store dentacoin, dentacoin wallet, pay with dentacoin'});
+        this.meta.updateTag({type: 'og:title', content: 'Dentacoin Wallet App: Buy, Store & Manage Your DCN Tokens'});
+        this.meta.updateTag({type: 'og:description', content: 'Dentacoin Wallet allows users to easily and securely store, send, receive DCN tokens, as well as to buy DCN with credit card and other cryptocurrencies.'});
+        this.meta.updateTag({type: 'og:image', content: 'https://wallet.dentacoin.com/assets/images/dentacoin-wallet-dapp.png'});
     }
 
     ngAfterViewInit() {
